590a3d2aaba7407b26ef3f62ac39220747cf242e
Rootjang92 <janggeunho7@gmail.com>
2018-12-27 13:14:20 +0900

    commit message

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -46,9 +46,27 @@ exports.write = async (ctx) => {
 };
 
 exports.list = async (ctx) => {
+  const page = parseInt(ctx.query.page || 1, 10);
+
+  if(page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    const posts = await Post.find()
+      .sort({_id: -1})
+      .limit(10)
+      .skip((page - 1) * 10)
+      .lean()
+      .exec();
+    const postCount = await Post.count().exec();
+    const limitBodyLength = post => ({
+      ...post,
+      body: post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`
+    });
+    ctx.body = posts.map(limitBodyLength);
+    ctx.set('Last-page', Math.ceil(postCount / 10));
   } catch(e) {
     ctx.throw(e, 500);
   }
